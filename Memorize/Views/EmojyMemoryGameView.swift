0d6e31cd1761ee46da5c55e281a0471b01a0c8b1
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-24 20:58:00 +0400

    + Animating emojis inside the card

--- Memorize/Views/EmojyMemoryGameView.swift
+++ Memorize/Views/EmojyMemoryGameView.swift
@@ -40,26 +40,30 @@ struct EmojyMemoryGameView_Previews: PreviewProvider {
 //MARK: --> SubViews
 struct CardView: View {
     let card: EmojiMemoryGame.Card
-
+    
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                    Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 20))
-                        .padding(5)
-                        .opacity(0.5)
-                    Text(card.content)
-                        .font(font(in: geometry.size))
+                Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 20))
+                    .padding(5)
+                    .opacity(0.5)
+                Text(card.content)
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(.linear(duration: 0.8).repeatForever(autoreverses: false), value: card.isMatched)
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private struct DrawingConstants {
         static let fontScale: CGFloat = 0.68
+        static let fontSize: CGFloat = 32
     }
 }
