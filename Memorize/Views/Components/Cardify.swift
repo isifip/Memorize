0c518298bbe19c218cdbfa14d052e15f3662fc12
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-24 19:59:35 +0400

    + Cardify ViewModifier

---
+++ Memorize/Views/Components/Cardify.swift
@@ -0,0 +1,37 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Irakli Sokhaneishvili on 24.01.22.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.CornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                content
+            } else {
+                shape.fill()
+            }
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let CornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
--- Memorize/Views/EmojyMemoryGameView.swift
+++ Memorize/Views/EmojyMemoryGameView.swift
@@ -45,21 +45,13 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.CornerRadius)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
                     Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 20))
                         .padding(5)
                         .opacity(0.5)
                     Text(card.content)
                         .font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
-                }
             }
+            .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
@@ -68,8 +60,6 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let CornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.68
     }
 }
