5dd65ecce4c2fde9b5f3453bb668e5daad7e0a77
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-25 14:41:30 +0400

    + Calculating Bonus time and animating in the view

--- Memorize/Models/MemoryGame.swift
+++ Memorize/Models/MemoryGame.swift
@@ -45,10 +45,70 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp = false
-        var isMatched = false
+        var isFaceUp = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
         let content: CardContent
         let id: Int
+        
+        
+        // This could give matching bonus points
+        // If the user matches the card
+        // Before a certain amount of time passes during which the card is face up
+        
+        // Can be zero which means "no bonus available" for the card
+        var bonusTimeLimit: TimeInterval = 12
+        
+        // How long this card has ever been face up
+        private var faceUptime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        // The last time this card was turned face up (and is still face up)
+        var lastFaceUpDate: Date?
+        
+        var pastFaceUpTime: TimeInterval = 0
+        
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUptime)
+        }
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+        }
+        
+        var hasEarbedBonus: Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        // Called when card transitions to face up state
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        // Called when the card goes back face down (or gets matched)
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUptime
+            self.lastFaceUpDate = nil
+        }
     }
 }
 
--- Memorize/Views/EmojyMemoryGameView.swift
+++ Memorize/Views/EmojyMemoryGameView.swift
@@ -115,7 +115,7 @@ struct EmojyMemoryGameView: View {
         } label: {
             Text("Restart")
         }
-
+        
     }
     
     private struct CardConstants {
@@ -142,13 +142,26 @@ struct EmojyMemoryGameView_Previews: PreviewProvider {
 struct CardView: View {
     let card: EmojiMemoryGame.Card
     
+    @State private var animatedBonusRemaining: Double = 0
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 20))
-                    .padding(5)
-                    .opacity(0.5)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: (1-animatedBonusRemaining)*360-90))
+                            .onAppear {
+                                animatedBonusRemaining = card.bonusRemaining
+                                withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                                    animatedBonusRemaining = 0
+                                }
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: -90), endAngle: Angle(degrees: (1-card.bonusRemaining)*360-90))
+                    }
+                }
+                .padding(5)
+                .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
                     .animation(.linear(duration: 0.8).repeatForever(autoreverses: false), value: card.isMatched)
